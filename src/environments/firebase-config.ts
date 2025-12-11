c6cac170006725a79754f23487dc00efe9228b78
georgesbyona <georgesbyona@gmail.com>
2025-12-11 06:43:50 +0000

    NPM Install for new config

--- src/environments/firebase-config.ts
+++ src/environments/firebase-config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyBIjcbwQW-BclmMmFGv6VyS3QMRx-nNVLc",
-  authDomain: "pata-lost.firebaseapp.com",
-  projectId: "pata-lost",
-  storageBucket: "pata-lost.appspot.com",
-  messagingSenderId: "100316297418",
-  appId: "1:100316297418:web:a319e48ed3b16c67d81c33"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID
 };
